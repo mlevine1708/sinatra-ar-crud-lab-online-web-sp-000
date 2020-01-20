4e9b20d2519f58a0a3a9b8c2293fd2b22cafef22
Micah Levine <micah.levine@outlook.com>
2020-01-20 19:44:30 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,46 +12,5 @@ class ApplicationController < Sinatra::Base
     redirect to "/articles"
   end
 
-  get '/posts/new' do
-    erb :new
-  end
-
-  get '/posts' do
-    @posts = Post.all
-
-    erb :index
-  end
-
-  post '/posts' do
-    @post = Post.create(params[:post])
-
-    erb :show
-  end
-
-  get '/posts/:id' do
-    @post = Post.find(params[:id])
-
-    erb :show
-  end
-
-  get '/posts/:id/edit' do
-    @post = Post.find(params[:id])
-
-    erb :edit
-  end
-
-  patch '/posts/:id' do
-    @post = Post.find(params[:id])
-    @post.update(params[:edit_post])
-    @post.save
-
-    erb :show
-  end
-
-  delete '/posts/:id/delete' do
-    @post = Post.find(params[:id])
-    @post.destroy
-
-    erb :delete
-  end
+  
 end
